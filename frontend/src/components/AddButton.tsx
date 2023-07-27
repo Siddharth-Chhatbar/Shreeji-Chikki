250a2fcae3bcf7be631c65d41d095ec4ec0fb5c4
Siddharth-Chhatbar <chhatbar@ualberta.ca>
2023-07-27 10:17:51 +0530

    Added components

---
+++ frontend/src/components/AddButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AddButton = () => {
+    return (
+        <div className='add-button'>
+            <div className=' button-text font-plein'>
+                Add
+            </div>
+        </div>
+    )
+}
+
+export default AddButton
---
+++ frontend/src/components/DownloadButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DownloadButton = () => {
+    return (
+        <div className=' button'>
+            <div className=' button-text font-plein'>
+                Download
+            </div>
+        </div>
+    )
+}
+
+export default DownloadButton
